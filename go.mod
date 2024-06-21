6ebebaef3eb760d1735060d3e41ff185326cbaf6
thaodtp <dtpthao.its@gmail.com>
2024-06-21 16:46:11 +0700

    add middleware to user handler

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gin-gonic/gin v1.10.0
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/google/uuid v1.6.0
+	github.com/ilyakaznacheev/cleanenv v1.5.0
 	github.com/rs/zerolog v1.33.0
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 	gorm.io/driver/mysql v1.5.7
@@ -13,6 +14,7 @@ require (
 )
 
 require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
@@ -26,6 +28,7 @@ require (
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
@@ -43,4 +46,5 @@ require (
 	golang.org/x/text v0.15.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
--- main.go
+++ main.go
@@ -1,35 +1,57 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/ilyakaznacheev/cleanenv"
 	"glintecoTask/entity"
-	usecase2 "glintecoTask/module/token/usecase"
+	tokenDelivery "glintecoTask/module/token/delivery"
+	tokenUC "glintecoTask/module/token/usecase"
 	"glintecoTask/module/user/delivery"
 	"glintecoTask/module/user/repository"
 	"glintecoTask/module/user/usecase"
+	apiLog "glintecoTask/utils/log"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 	"log"
 )
 
-func main() {
+var logConfig apiLog.Config
+var mysqlConfig entity.MysqlConfig
+var serverConfig entity.ServerConfig
 
-	// todo read config instead of hardcode
-	mysqlConfig := entity.MysqlConfig{
+func config() {
+	err := cleanenv.ReadEnv(&logConfig)
+	if err != nil {
+		log.Fatalf("Read log config failed. %v", err)
+	}
+
+	err = apiLog.InitLog(logConfig)
+	if err != nil {
+		log.Fatalf("Init log failed. %v", err)
+	}
+
+	// todo no hardcode
+	mysqlConfig = entity.MysqlConfig{
 		Host:     "localhost",
-		Port:     3306,
+		Port:     3307,
 		User:     "glinteco",
 		Password: "glinteco@123",
-		DBName:   "glinteco_db",
+		DBName:   "glintecodb",
 	}
 
-	serverConfig := entity.ServerConfig{
+	serverConfig = entity.ServerConfig{
 		Host:      "localhost",
 		Port:      3030,
-		JWTSecret: "",
+		JWTSecret: "5b0b18dc37004b97946367ca5d82673918a6c6e7a817bf84236abe1c0907b9bf",
 	}
+}
+
+func main() {
+
+	config()
 
 	dns := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", mysqlConfig.User, mysqlConfig.Password, mysqlConfig.Host, mysqlConfig.Port, mysqlConfig.DBName)
 	gormConfig := gorm.Config{}
@@ -47,16 +69,26 @@ func main() {
 	sqlDB.SetMaxIdleConns(10)
 	sqlDB.SetMaxOpenConns(100)
 
+	jwtSecret, _ := hex.DecodeString(serverConfig.JWTSecret)
+	tokenUseCase := tokenUC.NewTokenUseCase(jwtSecret)
+
 	app := gin.New()
+	app.Use(apiLog.AccessLog)
+
 	userRepository := repository.NewUserRepository(db)
-	tokenUseCase := usecase2.NewTokenUseCase(serverConfig.JWTSecret, userRepository)
 	userUseCase := usecase.NewUserUseCase(userRepository, tokenUseCase)
+
+	tokenHandler := tokenDelivery.NewTokenHandler(tokenUseCase, userUseCase, jwtSecret)
+
 	userHandler := delivery.NewUserHandler(app, userUseCase)
-	userHandler.RegisterHandler()
+	userHandler.RegisterHandler(tokenHandler.Authenticate, tokenHandler.AdminAuthorize)
 
 	addr := fmt.Sprintf("%s:%d", serverConfig.Host, serverConfig.Port)
+	apiLog.Info().Msg("Server start at: http://" + addr)
 	err = app.Run(addr)
+
 	if err != nil {
 		panic(err)
 	}
+
 }
